e23a05da547ae39984c0374b645f63844827343b
ejissyntaxerror <164166792+ejissyntaxerror@users.noreply.github.com>
2026-09-19 11:03:36 +0800

    create basic appointment system

---
+++ program/main.py
@@ -0,0 +1,8 @@
+print("ONLINE ID PRODUCTION APPOINTMENT SYSTEM")
+
+name = input("Enter your name: ")
+student_id = input("Enter your student ID: ")
+
+print("\nAppointment Details")
+print("Name:", name)
+print("Student ID:", student_id)
